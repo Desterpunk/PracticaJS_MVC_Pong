e2e80fd9efb3d345d2adcdd012c658e6b3fc7e41
Jhon Serna <jhonalfaber@gmail.com>
2021-09-08 11:27:47 -0500

    Agregando linea divisora en main.js

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 const canvas = document.getElementById("pong");
 const ctx = canvas.getContext('2d');
 
+const halfLine = {
+    x : (canvas.width - 2)/2,
+    y : 0,
+    height : 10,
+    width : 2,
+    color : "WHITE"
+}
+
+function drawHalfLine(){
+    for(let i = 0; i <= canvas.height; i+=15){
+        drawRectangle(halfLine.x, halfLine.y + i, halfLine.width, halfLine.height, halfLine.color);
+    }
+}
+
 function drawRectangle(x, y, w, h, color){
     ctx.fillStyle = color;
     ctx.fillRect(x, y, w, h);
@@ -8,6 +22,7 @@ function drawRectangle(x, y, w, h, color){
 
 function render(){
     drawRectangle(0, 0, canvas.width, canvas.height, "#800");
+    drawHalfLine();
 }
 function update(){
 

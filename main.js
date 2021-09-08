6d08b5b7944711fa11ce321110e1b0b09d084aff
Jhon Serna <jhonalfaber@gmail.com>
2021-09-08 12:25:52 -0500

    Creando carpeta views con fieldView para guardar metodos implementados referentes al campo

--- main.js
+++ main.js
@@ -1,28 +1,11 @@
+import { fieldView } from "./views/fieldView.js";
+
 const canvas = document.getElementById("pong");
-const ctx = canvas.getContext('2d');
-
-const halfLine = {
-    x : (canvas.width - 2)/2,
-    y : 0,
-    height : 10,
-    width : 2,
-    color : "WHITE"
-}
-
-function drawHalfLine(){
-    for(let i = 0; i <= canvas.height; i+=15){
-        drawRectangle(halfLine.x, halfLine.y + i, halfLine.width, halfLine.height, halfLine.color);
-    }
-}
-
-function drawRectangle(x, y, w, h, color){
-    ctx.fillStyle = color;
-    ctx.fillRect(x, y, w, h);
-}
 
 function render(){
-    drawRectangle(0, 0, canvas.width, canvas.height, "#800");
-    drawHalfLine();
+    const field = new fieldView(0, 0, canvas.width, canvas.height, "#800",canvas);
+    field.drawRectangle(0, 0, canvas.width, canvas.height, "#800");
+    field.drawHalfLine();
 }
 function update(){
 
---
+++ views/fieldView.js
@@ -0,0 +1,31 @@
+class fieldView{
+    constructor(x, y,width,height, color,canvas){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.canvas = canvas;
+        this.ctx = canvas.getContext('2d');
+    }
+
+    drawRectangle(x, y, w, h, color){
+        this.ctx.fillStyle = color;
+        this.ctx.fillRect(x, y, w, h);
+    }
+    
+    drawHalfLine(){
+        const halfLine = {
+            x : (this.canvas.width - 2)/2,
+            y : 0,
+            height : 10,
+            width : 2,
+            color : "WHITE"
+        }
+        for(let i = 0; i <= this.canvas.height; i+=15){
+            this.drawRectangle(halfLine.x, halfLine.y + i, halfLine.width, halfLine.height, halfLine.color);
+        }
+    }
+}
+
+export {fieldView}

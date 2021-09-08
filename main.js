823a272ae92d086052b9af4fccb28d87667d7bf9
Jhon Serna <jhonalfaber@gmail.com>
2021-09-08 18:34:40 -0500

    Boton para iniciar el juego implementado en index.html y en main.js se pone a la escucha

--- main.js
+++ main.js
@@ -3,17 +3,18 @@ import { player } from "./models/player.js";
 import { ball } from "./models/ball.js";
 
 const canvas = document.getElementById("pong");
+let buttonStart = document.querySelector('button');
 
 const field = new fieldView(canvas);
 const player1 = new player(0,(canvas.height - 100)/2,10,100,0,"white");
 const player2 = new player(canvas.width - 10,(canvas.height - 100)/2,10,100,0,"white");
-const ball1 = new ball(canvas.width/2,canvas.height/2,10,5,5,7,"white");
+const ball1 = new ball(canvas.width/2,canvas.height/2,10,5,5,"white");
 
 canvas.addEventListener("mousemove", getMousePos);
 function getMousePos(evt){
     let rect = canvas.getBoundingClientRect();
     player1.y = parseInt(evt.clientY - rect.top - player1.heigth/2);
-}
+}   
 
 function render(){
     field.drawRectangle(0, 0, canvas.width, canvas.height, "#800");
@@ -24,8 +25,38 @@ function render(){
     field.drawText(player1.score,canvas.width/4,canvas.height/5);
     field.drawText(player2.score,(canvas.width/4)*3,canvas.height/5);
 }
+
+
+
 function update(){
-    console.log(player2.y)
+    if( ball1.x - ball1.radius < -20 ){
+        player2.score++;
+        ball1.resetBall();
+    }else if( ball1.x + ball1.radius > canvas.width+20){
+        player1.score++;
+        ball1.resetBall();
+    }
+
+    ball1.x += ball1.velocityX;
+    ball1.y += ball1.velocityY;
+    player2.y += ((ball1.y-(player2.y+(player2.heigth/2)))*0.1);
+
+    if(ball1.y - ball1.radius < 0 || ball1.y + ball1.radius > canvas.height){
+        ball1.velocityY = -ball1.velocityY;
+    }
+
+    if(ball1.x < player1.x && ball1.y > player1.y && ball1.y < player1.y+100){
+        ball1.velocityX = -ball1.velocityX;
+        ball1.speedExtra();
+    }
+    else if (ball1.x > player2.x && ball1.y > player2.y && ball1.y < player2.y+100){
+        ball1.velocityX = -ball1.velocityX;
+        ball1.speedExtra();
+    } 
+    
+    if (player2.score > 3){
+        console.log("perdedor");
+    }
 }
 
 function game(){
@@ -33,5 +64,12 @@ function game(){
     update();
 }
 
-let framePerSecond = 50;
-let loop = setInterval(game,1000/framePerSecond);
+let framePerSecond = 60;
+let loop = setInterval(game,1000/framePerSecond); 
+
+buttonStart.addEventListener('click', () => {
+  
+    ball1.resetBall();
+    player1.score=0;
+    player2.score=0;
+});
--- models/ball.js
+++ models/ball.js
@@ -1,13 +1,28 @@
 class ball{
-    constructor(x,y,radius,velocityX,velocityY,speed,color){
+    constructor(x,y,radius,velocityX,velocityY,color){
         this.x = x;
         this.y = y;
         this.radius = radius;
         this.velocityX = velocityX;
         this.velocityY = velocityY;
-        this.speed = speed;
         this.color = color;
     }
+
+    resetBall(){
+        this.x = 300;
+        this.y = 200;
+        this.velocityX = 5;
+        this.velocityY = 5;
+    }
+
+    speedExtra(){
+        if(this.velocityX > 0){
+            this.velocityX += 0.1;
+        } else {
+            this.velocityX -= 0.1;
+        }
+    }
+
 }
 
 export{ball};

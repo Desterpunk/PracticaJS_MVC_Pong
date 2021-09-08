33a32877fef10745b232a6cc56986c923dfad230
Jhon Serna <jhonalfaber@gmail.com>
2021-09-08 11:18:57 -0500

    dibujo de rectangulo en main.js sobre el cual trabajar

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 const canvas = document.getElementById("pong");
 const ctx = canvas.getContext('2d');
 
+function drawRectangle(x, y, w, h, color){
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, w, h);
+}
+
 function render(){
+    drawRectangle(0, 0, canvas.width, canvas.height, "#800");
+}
+function update(){
+
 }
 
 function game(){
+    render();
+    update();
 }
 
 let framePerSecond = 50;
